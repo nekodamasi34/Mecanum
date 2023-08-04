64bbb7d3606de5faed93e58c5dce68b632ab7baf
nekodamasi34 <117162125+nekodamasi34@users.noreply.github.com>
2023-08-05 00:21:04 +0900

    Update md.hpp

--- md.hpp
+++ md.hpp
@@ -7,8 +7,8 @@
 class MD
 {
     public:
-    MD(PinName pwm_pin, PinName dir_pin)
-    : pwm(pwm_pin), dir(dir_pin)
+    MD(PinName pwm_pin, PinName dir_pin, bool reverse)
+    : pwm(pwm_pin), dir(dir_pin), reverse(reverse)  //(pwmピン, dirピン, 逆転モード)
     {
         pwm.period(0.0005);
     };
@@ -20,14 +20,20 @@ class MD
 
     void drive(float power)
     {   
+        if(reverse){
+            power *= -1;
+        }
+
         dir = power < 0 ? 0 : 1;
         pwm = abs(power) > 1 ? 1.0 : abs(power);
         // a ? b : c aが真ならbが実行、aが偽ならcが実行。
+        
     }
 
     private:
         PwmOut pwm;
         DigitalOut dir;
+        bool reverse;
 };
 
 #endif
