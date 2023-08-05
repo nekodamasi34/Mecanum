4b2b280c69ca3b796784e839fce284ed43975811
Tetsuya Yamamoto <49048811+testusuke@users.noreply.github.com>
2023-08-05 23:46:15 +0900

    refactor: private variable naming
    in case of private variable, using underscore before its name.
    
    before:
    double wheel[]
    
    after:
    double _wheel[]

--- Mecanum.hpp
+++ Mecanum.hpp
@@ -8,7 +8,7 @@ class MecanumWheel {
 
 private:
 
-    double wheel[4];
+    double _wheel[4];
 
 public:
 
@@ -17,17 +17,17 @@ public:
 
     void control(double _targetSpeed, double _targetRotation, double _LRturn)
     {
-        wheel[0] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed - _targetRotation;
-        wheel[1] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed + _targetRotation;
-        wheel[2] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed - _targetRotation;
-        wheel[3] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed + _targetRotation;
+        _wheel[0] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed - _targetRotation;
+        _wheel[1] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed + _targetRotation;
+        _wheel[2] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed - _targetRotation;
+        _wheel[3] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed + _targetRotation;
     }
 
     double getSpeed(int unit) {
         if(unit < 0 || unit > 3) {
             return 0.0;
         }
-        return wheel[unit];
+        return _wheel[unit];
     }
 };
 
